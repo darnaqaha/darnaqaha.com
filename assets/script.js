498265f93905436b0a53c2c3393e85ae246c1bb1
Robert Moritz <robert@nott-studios.co.uk>
2018-04-26 11:41:49 +0100

    lang switcher

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,37 @@ var langSwitcher = function(e) {
 			.forEach(function(element) {
 				element.classList.remove('hidden');
 			});
-	} else if (e.value == "pl") {
-		Array.prototype.slice.call(document.querySelectorAll('[lang="pl"]'))
+		Array.prototype.slice.call(document.querySelectorAll('[lang="fr"]'))
+			.forEach(function(element) {
+				element.classList.add('hidden');
+			});
+		Array.prototype.slice.call(document.querySelectorAll('[lang="ar"]'))
+			.forEach(function(element) {
+				element.classList.add('hidden');
+			});
+	} else if (e.value == "fr") {
+		Array.prototype.slice.call(document.querySelectorAll('[lang="en"]'))
+			.forEach(function(element) {
+				element.classList.add('hidden');
+			});
+		Array.prototype.slice.call(document.querySelectorAll('[lang="fr"]'))
+			.forEach(function(element) {
+				element.classList.remove('hidden');
+			});
+		Array.prototype.slice.call(document.querySelectorAll('[lang="ar"]'))
+			.forEach(function(element) {
+				element.classList.add('hidden');
+			});
+	} else if (e.value == "ar") {
+		Array.prototype.slice.call(document.querySelectorAll('[lang="en"]'))
+			.forEach(function(element) {
+				element.classList.add('hidden');
+			});
+		Array.prototype.slice.call(document.querySelectorAll('[lang="fr"]'))
+			.forEach(function(element) {
+				element.classList.add('hidden');
+			});
+		Array.prototype.slice.call(document.querySelectorAll('[lang="ar"]'))
 			.forEach(function(element) {
 				element.classList.remove('hidden');
 			});
