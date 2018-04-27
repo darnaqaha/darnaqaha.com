3608e4e710b9ae47bce9714d763c54b30953e50b
Robert Moritz <robert@nott-studios.co.uk>
2018-04-27 07:48:22 +0100

    header

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
+// Setting localStorage of the selected language - used by the language switcher
 var langSet = function(e) {
 	localStorage.setItem('dnq_lang', e.innerText);
 }
 
+// Function run on start or change of language to show the selected language - taken from the localStorage
 var langSwitcher = function() {
 	e = localStorage.getItem("dnq_lang");
 	if (e == "En") {
@@ -47,3 +49,13 @@ var langSwitcher = function() {
 	console.log("Language picked: " + e);
 }
 document.addEventListener('DOMContentLoaded', langSwitcher(), false);
+
+// Header logo changer on scroll
+document.addEventListener("scroll", function() {
+	if ( window.scrollY > 15 ) {
+		document.getElementById('header-logo').classList.add('logo-scrolled');
+	}
+	else {
+		document.getElementById('header-logo').classList.remove('logo-scrolled');
+	}
+} );
